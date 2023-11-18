6c7559eba67f38719108d5067893e1a3f8fcfde7
Devin Shoemaker <devinshoe@gmail.com>
2023-11-18 15:36:43 -0600

    update migration to auto crate and destroy users

--- supabase/migrations/20231118211130_users_table.sql
+++ supabase/migrations/20231118211130_users_table.sql
@@ -4,12 +4,36 @@ create table "public"."users" (
     "created_at" timestamp with time zone not null default now()
 );
 
-
 alter table "public"."users" enable row level security;
 
 CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
 
+CREATE UNIQUE INDEX user_email_key ON public.users USING btree (email);
+
 alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "user_email_key" UNIQUE using index "user_email_key";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO 'public'
+AS $function$
+begin
+  insert into public.users (id, email)
+  -- Reference the newly created `id`
+  values (new.id, new.email);
+  return new;
+end;
+
+-- Return the function
+$function$
+;
+
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
 
 create policy "Public profiles are viewable by everyone."
   on users for select
