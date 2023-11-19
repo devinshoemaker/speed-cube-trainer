239dd58f569027927eb55778deaa77ff8ca6c6a9
Devin Shoemaker <devinshoe@gmail.com>
2023-11-19 14:13:57 -0600

    chore: add side menu

--- apps/speed-cube-trainer/app/layout.tsx
+++ apps/speed-cube-trainer/app/layout.tsx
@@ -1,5 +1,8 @@
 import { GeistSans } from 'geist/font/sans';
+import { cookies } from 'next/headers';
 
+import SideMenu from '../components/SideMenu';
+import { createClient } from '../utils/supabase/server';
 import './globals.css';
 
 const defaultUrl = process.env.VERCEL_URL
@@ -12,15 +15,23 @@ export const metadata = {
   description: 'The fastest way to build apps with Next.js and Supabase',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const {
+    data: { user: isAuthenticated },
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+      <body className="relative min-h-screen md:flex text-foreground w-full bg-background">
+        {isAuthenticated && <SideMenu />}
+        <main className="min-h-screen w-full flex flex-col items-center">
           {children}
         </main>
       </body>
