47e7607493c0cc241080ecd96e278e40a0df60e8
Devin Shoemaker <devinshoe@gmail.com>
2023-11-19 13:22:50 -0600

    feat: add timer page (#11)

---
+++ apps/speed-cube-trainer-e2e/lib/create-user.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { expect, Page } from '@playwright/test';
+
+export async function createUser(page: Page) {
+  const email = faker.internet.email();
+  const password = faker.internet.password();
+
+  await page.goto('/login');
+
+  // Create account
+  await page.getByLabel(/Email/i).fill(email);
+  await page.getByLabel(/Password/i).fill(password);
+  await page.getByText(/Sign Up/i).click();
+  // Wait for sign up to be complete
+  await expect(
+    page.getByText(/Check email to continue sign in process/i)
+  ).toBeVisible();
+  // Sign in
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  // Verify login was successful
+  await expect(page.getByText(`Hey, ${email}!`)).toBeVisible();
+}
---
+++ apps/speed-cube-trainer-e2e/src/timer.spec.ts
@@ -0,0 +1,24 @@
+import test, { expect } from '@playwright/test';
+
+import { createUser } from '../lib/create-user';
+
+test.describe('Timer', () => {
+  test('should start the timer with the spacebar', async ({ page }) => {
+    await createUser(page);
+    await page.goto('/timer');
+    await expect(page.getByText('00:00:00')).toBeVisible();
+    await page.keyboard.press('Space');
+    await page.waitForTimeout(100);
+    await page.keyboard.press('Space');
+    await expect(page.getByTestId('timer')).not.toContainText('00:00:00');
+  });
+
+  test('should start the timer by clicking the screen', async ({ page }) => {
+    await createUser(page);
+    await page.goto('/timer');
+    await page.getByText('00:00:00').click();
+    await page.waitForTimeout(100);
+    await page.getByTestId('timer').click();
+    await expect(page.getByTestId('timer')).not.toContainText('00:00:00');
+  });
+});
