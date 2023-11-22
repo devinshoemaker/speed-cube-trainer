2bb398359fc34f99056b99c850ecac0639c351b3
Devin Shoemaker <devinshoe@gmail.com>
2023-11-22 08:59:09 -0600

    fix: timer link in side menu navigates correctly (#13)

--- apps/speed-cube-trainer-e2e/src/timer.spec.ts
+++ apps/speed-cube-trainer-e2e/src/timer.spec.ts
@@ -3,6 +3,12 @@ import test, { expect } from '@playwright/test';
 import { createUser } from '../lib/create-user';
 
 test.describe('Timer', () => {
+  test('side menu should navigate to timer', async ({ page }) => {
+    await createUser(page);
+    await page.getByText(/Timer/i).click();
+    await expect(page).toHaveURL('/timer');
+  });
+
   test('should start the timer with the spacebar', async ({ page }) => {
     await createUser(page);
     await page.goto('/timer');
